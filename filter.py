e0ce2c548dcbab09bb61d408cad7779a71e44eee
pingbowen <pingbowen23@163.com>
2024-08-27 09:52:39 +0800

    support filter copyright,exam et al.

--- filter.py
+++ filter.py
@@ -1,6 +1,7 @@
 import json
 import re
 import argparse
+from openai import OpenAI
 
 # 定义正则表达式，用于检测http或https链接
 url_pattern = re.compile(r'(https?://|www\.)[^\s]+')
@@ -8,12 +9,6 @@ ref_pattern = re.compile(r'REF\d+FIG')
 special_str_pattern = '> > > > >'
 dictionary_entry_pattern = re.compile(r'\n\n(.*?)\n\n', re.DOTALL)
 exam_pattern = re.compile(r'\d+\.\s+[^\n]+\n(?:[a-d]\)\s+[^\n]+\n)+')
-
-
-# 输入文件路径
-input_file = '/home/pingbowen/workspace/DDPO/Self-genertor_segment_dpo/data/output_32k+.jsonl'
-# 输出文件路径
-output_file = './data/test_32k+.jsonl'
 
 def count_chinese_english_matches(input_text):
     # 定义正则表达式，用于匹配相邻 \n\n 之间的内容
@@ -77,22 +72,60 @@ def count_chinese_and_english(text):
     
     return chinese_count, english_count
 
-def contains_exam_format(text):
-    return bool(re.search(exam_pattern, text))
+def clean_exam_bilingual_format(text):
+    
+    client = OpenAI(
+        api_key='sk-TMDtm8NkAIaXwgS03a80CbAa21C940B793348993FbB288Ed',
+        base_url='https://yeysai.com/v1/'
+    )
+    
+    text = f"""
+    You are given a text, and your task is to determine whether the main content of this text is a test question consisting of a question and several options, or whether it is a bilingual book with alternating English and Chinese sentences.\n\nYou should only output "yes" or "no" without providing any unrelated content.
+    ### Input text
+    {text[:4096]}
+    ### The text ends here. Please, give your response.
+    """
+    
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo", # 
+        messages=[
+            {"role": "user", "content": text}
+        ]
+    )
+    
+    return completion.choices[0].message.content
 
+
+def retain_text_from_special_word(text):
+    # 正则表达式模式，匹配首字母大写且首字母两边带有双星号的单词
+    pattern = r'\*\*[A-Z]\*\*[a-zA-Z]*'
+    
+    # 查找第一个符合条件的单词及其在文本中的位置
+    match = re.search(pattern, text)
+    
+    if match:
+        # 保留从第一个匹配开始的所有内容
+        result_text = text[match.start():]
+        return result_text
+    else:
+        return text
 
 
 if __name__ == '__main__':
-    # with open("/home/pingbowen/workspace/DDPO/Self-genertor_segment_dpo/data/output_32k+.jsonl", 'r', encoding='utf-8') as file:
+    # with open("/home/pingbowen/workspace/DDPO/Self-genertor_segment_dpo/Index_data/data.jsonl", 'r', encoding='utf-8') as file:
     #     for idx,line in enumerate(file):
     #         data = json.loads(line)
-    #         response = data.get('response', '') # response
+    #         words = data["output"]
     #         import pdb; pdb.set_trace()
     #         a = 1
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--language', type=str, default='data.jsonl')
-    # argparser.add_argument('--threshold', type=float, default=0.06)
+    argparser.add_argument('--input_file', type=str, default="")
+    argparser.add_argument('--output_file', type=str, default="")
     args = argparser.parse_args()
+    
+    input_file = args.input_file 
+    output_file = args.output_file
     
     threshold = 0.06 if args.language == 'en' else 0.04
     
@@ -102,21 +135,17 @@ if __name__ == '__main__':
             data = json.loads(line)
             response = data.get('response', '') # response
             
-            ref_count = len(re.findall(ref_pattern, response))
-            special_count = response.count(special_str_pattern)
-            chinese_count, english_count = count_chinese_and_english(response)
+            response = retain_text_from_special_word(response) # 出去版权声明、参考文献等特殊词汇
+            
+            ref_count = len(re.findall(ref_pattern, response)) # 清理refXfig
+            special_count = response.count(special_str_pattern) # 清理">>>>>"
+            chinese_count, english_count = count_chinese_and_english(response) # 统计中文和英文字符数
             if args.language == 'en':
-                # try:
                 ratio = chinese_count / (chinese_count + english_count)
-                # except ZeroDivisionError:
-                #     continue
             else:
                 ratio = english_count / (chinese_count + english_count)
             
-            # sub_counts , count_both = count_chinese_english_matches(response)
-            # binlingual = contains_chinese_and_english(response)
-            # import pdb; pdb.set_trace()
-            if not re.search(url_pattern, response) and ref_count < 5 and special_count < 3 and ratio < threshold : #and 
+            if not re.search(url_pattern, response) and ref_count < 3 and special_count < 3 and ratio < threshold : #and 
                 outfile.write(line)
 
     print(f"过滤后的数据已保存到 {output_file}")
--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 numbers=(2000 1500 1500)
 len_min=(0 10000 20000)
 len_max=(10000 20000 32000)
-for i in {0..0}
-do
-    python Back_trans.py --number ${numbers[$i]} \
-    --context_len_min ${len_min[$i]} \
-    --context_len_max ${len_max[$i]}
-done
+# for i in {0..0}
+# do
+#     python Back_trans.py --number ${numbers[$i]} \
+#     --context_len_min ${len_min[$i]} \
+#     --context_len_max ${len_max[$i]}
+# done
+
+python3 filter.py --language en \
+ --input_file /home/pingbowen/workspace/DDPO/Self-genertor_segment_dpo/data/output_32k+.jsonl \
+ --output_file ./data/test_32k+.jsonl \ 
